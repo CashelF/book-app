10cd027f059ee00a6552a83890f3292d82c8165d
David Pekar <davidpekar@utexas.edu>
2024-04-13 19:20:16 -0500

    Initial Home Page Done

---
+++ frontend/screens/HomeScreen.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { View, Text, Button, Image,TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import SearchBar from "react-native-dynamic-search-bar";
+import BookGrid from '../components/BookListEx';
+
+const HomeScreen = () => {
+    return (
+        <View style={styles.everythingContainer}>
+            <View style={styles.container}>
+            <View style={styles.menuContainer}>
+            <Ionicons name="menu" size={32} color="black" style={styles.menuIcon} />
+            <View style={styles.circleIcon}></View>
+            </View>
+            <View style={styles.header}>
+            <Text style={styles.welcomeText}>Welcome back, Bunny!</Text>
+            <Text style={styles.subHeaderText}>Here are your saved and suggested books!</Text>
+            </View>
+            <SearchBar
+                style={styles.searchBar}
+                placeholder="Search here"
+                onPress={() => alert("onPress")}
+                onChangeText={(text) => console.log(text)}
+            />
+            <View style={styles.buttonLayout}>
+                <TouchableOpacity style={{height:50,backgroundColor:"white",alignItems:'center',justifyContent:'center', paddingRight: 10}}>
+                    <Text style={{fontSize:16,}}>Saved</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{height:50,backgroundColor:"white",alignItems:'center',justifyContent:'center'}}>
+                    <Text style={{fontSize:16,}}>Suggested</Text>
+                </TouchableOpacity>
+            </View> 
+            </View>
+            <BookGrid/>
+            </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    paddingTop: 70,
+    paddingLeft: 30,
+    paddingRight: 30
+  },
+  menuContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  circleIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: 'black', 
+  },
+  welcomeText: {
+    color: "#9D9D9D",
+    fontSize: 16
+  },
+  subHeaderText: {
+    color: "#19191B",
+    fontSize: 26
+  },
+  searchBar: {
+    width: "100%",
+    marginTop: 20,
+    backgroundColor: 'rgba(196, 196, 196, 0.15)'
+  },
+  buttonLayout: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 10
+  },
+});
+
+export default HomeScreen;
