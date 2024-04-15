57368bb329625358afc6a2f7cf0b1ce428fb4d3b
Cashel <cashel@utexas.edu>
2024-04-15 12:26:20 -0500

    Include gunicorn suport

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,6 @@
 from app import create_app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0') ## debug=True is for development only!!!
+    app.run(host='0.0.0.0')  # debug=True should indeed only be for development!
