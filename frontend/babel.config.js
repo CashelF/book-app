2a57e164da8183204280390e130807b5db84452e
Cashel <cashel@utexas.edu>
2024-07-07 17:04:36 -0500

    Fix android support

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,10 +1,15 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-    }],
-  ],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+      }],
+    ],
+  };
 };
-
