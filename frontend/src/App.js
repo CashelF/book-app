9340ebc573bbd38d4c5e8eaded3b04259ef27f07
Cashel <cashel@utexas.edu>
2024-07-07 18:04:44 -0500

    Integrate web server in swipe page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import HomeScreen from './screens/HomeScreen';  
+import SwipingScreen from './screens/SwipingScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,7 +14,8 @@ function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        {/* <Stack.Screen name="Swiping" component={SwipingScreen} options={{ title: 'Books' }} /> */}
+        <Stack.Screen name="Home" component={SwipingScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
